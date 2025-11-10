c44a6ecd98672719211db47b4c3731bca59bcd9d
SaKkuSO10 <sa11kurarudan11@gmail.com>
2025-11-10 22:35:55 +0900

    feat: メモリーページ作成（途中まで

--- digigage/lib/pages/memory_gage.dart
+++ digigage/lib/pages/memory_gage.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class MemoryPage extends StatelessWidget {
+class MemoryPage extends StatefulWidget {
   const MemoryPage({super.key});
+  @override
+  State<MemoryPage> createState() => _MemoryPageState();
+}
+
+class _MemoryPageState extends State<MemoryPage> {
+  int memory = 0;
+  static const int minMem = -10;
+  static const int maxMem = 10;
+
+  double _alignXFromMemory(int v) {
+    final t = (v - minMem) / (maxMem - minMem);
+    return t * 2 -1;
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Memory')),
-      body: const Center(child: Text('Memory Page')),
-    );
+  Widget build(BuildContext context){
+    return Aspectratio(
+      aspectRation; 2.2,
+      child: Stack(
+        alignment: Alignment.center,
+        
+      )
+
+    )
   }
+  
+
+
 }
