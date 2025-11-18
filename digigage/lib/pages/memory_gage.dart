237e7514afc9479a09d339b6495e319c86bb9544
SaKkuSO10 <sa11kurarudan11@gmail.com>
2025-11-19 00:44:37 +0900

    feat:メモリーゲージのレイアウト変更１

--- digigage/lib/pages/memory_gage.dart
+++ digigage/lib/pages/memory_gage.dart
@@ -8,26 +8,30 @@ class MemoryPage extends StatefulWidget {
 
 class _MemoryPageState extends State<MemoryPage> {
   int memory = 0;
-  static const int minMem = -10;
-  static const int maxMem = 10;
 
-  double _alignXFromMemory(int v) {
-    final t = (v - minMem) / (maxMem - minMem);
-    return t * 2 -1;
-  }
+
+  static const int maxMem = 10;
+  static const int minMem = -10;
 
   @override
   Widget build(BuildContext context){
-    return Aspectratio(
-      aspectRation; 2.2,
-      child: Stack(
-        alignment: Alignment.center,
-        
-      )
+    return Scaffold(
+      Column(
+        children:(
+          Row(
+            children: List.generate(10, (i){
+              final display = 10-i,
+              final value = -(10- i);
+            }
+            }
 
-    )
-  }
-  
 
 
+
+          Row('0'),
+          Row('Player1'),
+        ),
+        )
+      ),
+  };
 }
